347a0481986499c8cd3bde281a959511af56ae8d
Reema Patel <reemapatel8189@gmail.com>
2026-07-22 10:15:24 +0530

    Updated MongoDB connection

--- final pr_recipe/config/db.js
+++ final pr_recipe/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(
+      "mongodb+srv://recipeDB:recipe@cluster0.fjwfiza.mongodb.net/RecipeDB?retryWrites=true&w=majority&appName=Cluster0"
+    );
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
